58b862ddfebb01852bfc429fa84ece477412e2f5
Jeongho Han <jungho1109@gmail.com>
2020-02-05 11:20:09 -0800

    Animation Initial

---
+++ anim/src/components/CSStransition.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "../css/App.css";
+
+class Fade extends Component {
+  render() {
+    return <div>CSS TRANSITIONS</div>;
+  }
+}
+
+export default Fade;
